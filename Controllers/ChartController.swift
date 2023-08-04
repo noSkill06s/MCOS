98a8102a8713631036d0b615cdaddf30535d0e2d
Cankurt Burhan <burhan.cankurt@icloud.com>
2023-08-04 05:55:27 +0200

    ChartController vollfunktionsfähig, Design und Buttons fertigzustellen

--- Controllers/ChartController.swift
+++ Controllers/ChartController.swift
@@ -10,6 +10,45 @@ import UIKit
 
 class ChartController: UIViewController, CPTBarPlotDataSource, CALayerDelegate {
     
+    enum TimeFrame {
+        case fiveMinutes
+        case fifteenMinutes
+        case thirtyMinutes
+        case oneHour
+        case fourHours
+        case oneDay
+    }
+
+    func filterData(for timeFrame: TimeFrame, data: [(date: String, close: Double)]) -> [(date: String, close: Double)] {
+        let now = Date()
+        var dateToCompare: Date
+
+        switch timeFrame {
+        case .fiveMinutes:
+            dateToCompare = now.addingTimeInterval(-5 * 60)
+        case .fifteenMinutes:
+            dateToCompare = now.addingTimeInterval(-15 * 60)
+        case .thirtyMinutes:
+            dateToCompare = now.addingTimeInterval(-30 * 60)
+        case .oneHour:
+            dateToCompare = now.addingTimeInterval(-60 * 60)
+        case .fourHours:
+            dateToCompare = now.addingTimeInterval(-4 * 60 * 60)
+        case .oneDay:
+            dateToCompare = Calendar.current.date(byAdding: .day, value: -1, to: now)!
+        }
+
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+
+        return data.filter { dataPoint in
+            if let date = dateFormatter.date(from: dataPoint.date) {
+                return date >= dateToCompare
+            }
+            return false
+        }
+    }
+
     var plotData: [(date: String, close: Double)] = []
     @IBOutlet var graphView: CPTGraphHostingView!
  
